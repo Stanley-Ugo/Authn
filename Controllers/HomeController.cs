f759b94de8b016bfdb5144487f8c8d3305974b46
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-05 15:12:44 +0100

    Adding a SecuredRotes Method in the HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Authn.Controllers
             return View();
         }
 
+        public IActionResult SecuredRoute()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
