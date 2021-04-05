1822b6f168ec3e10036c8f3067ec8f9a706f3704
Stanley-Ugo <iamKosmicblast@gmail.com>
2021-04-05 15:33:32 +0100

    Registering Authentication in the services

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace Authn
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddAuthentication();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
